77e9da6f585400ace46b75df8aec3f4280bc39c7
CaptainClaw1997 <bhari.elan@gmail.com>
2022-02-26 22:22:31 +0530

    Fix styling of password mismatch error message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const signUpForm = document.querySelector("form");
 const submitButton = document.querySelector("button#create-account");
 const passwordTextBox = document.querySelector("input#password");
 const confirmPasswordTextBox = document.querySelector("input#confirm-password");
+const passwordMismatchMessageElement = document.querySelector("#password-mismatch-error-message");
 
 passwordTextBox.onkeyup = () => {
     checkPasswordsMatch(passwordTextBox, confirmPasswordTextBox);
@@ -15,8 +16,10 @@ function checkPasswordsMatch(passwordTextBox, confirmPasswordTextBox) {
     if (passwordTextBox.value !== confirmPasswordTextBox.value) {
         passwordTextBox.classList.add("invalid");
         confirmPasswordTextBox.classList.add("invalid");
+        passwordMismatchMessageElement.style.setProperty("display", "block");
     } else {
         passwordTextBox.classList.remove("invalid");
         confirmPasswordTextBox.classList.remove("invalid");
+        passwordMismatchMessageElement.style.setProperty("display", "none");
     }
 }
