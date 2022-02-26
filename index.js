3e8eb79c81a88f08c97ef7dd1d74e680ca49fba5
CaptainClaw1997 <bhari.elan@gmail.com>
2022-02-26 22:07:12 +0530

    Correct mechanism to check password

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const submitButton = document.querySelector("button#create-account");
 const passwordTextBox = document.querySelector("input#password");
 const confirmPasswordTextBox = document.querySelector("input#confirm-password");
 
-signUpForm.onsubmit = () => {
+passwordTextBox.onkeyup = () => {
+    checkPasswordsMatch(passwordTextBox, confirmPasswordTextBox);
+}
+
+confirmPasswordTextBox.onkeyup = () => {
     checkPasswordsMatch(passwordTextBox, confirmPasswordTextBox);
 }
 
@@ -11,5 +15,8 @@ function checkPasswordsMatch(passwordTextBox, confirmPasswordTextBox) {
     if (passwordTextBox.value !== confirmPasswordTextBox.value) {
         passwordTextBox.classList.add("invalid");
         confirmPasswordTextBox.classList.add("invalid");
+    } else {
+        passwordTextBox.classList.remove("invalid");
+        confirmPasswordTextBox.classList.remove("invalid");
     }
 }
